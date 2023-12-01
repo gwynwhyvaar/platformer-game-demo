a0ace09ab06a557c2d6eb260ff09913840b4b4ab
Gwynwhyvaar <tunde.ayoko@hotmail.com>
2023-12-01 18:27:52 +0100

    code improvements

--- platformer_3d/Assets/PlayerMovement.cs
+++ platformer_3d/Assets/PlayerMovement.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody _rigidBody;
     // Start is called before the first frame update
     void Start()
     {
+        _rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -12,15 +14,23 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0); // since we are only jumping, then we only need to set the Y value of the vector3 structure
+            _rigidBody.velocity = new Vector3(0, 5, 0); // since we are only jumping, then we only need to set the Y value of the vector3 structure
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5); // since we are moving up, then we only need to set the Z value of the vector3 structure
+            _rigidBody.velocity = new Vector3(0, 0, 5); // since we are moving up, then we only need to set the Z value of the vector3 structure
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0); // since we are moving right, then we only need to set the YX value of the vector3 structure
+            _rigidBody.velocity = new Vector3(5, 0, 0); // since we are moving right, then we only need to set the YX value of the vector3 structure
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _rigidBody.velocity = new Vector3(0, 0, -5); // since we are moving right, then we only need to set the YX value of the vector3 structure
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rigidBody.velocity = new Vector3(-5, 0, 0); // since we are moving right, then we only need to set the YX value of the vector3 structure
         }
     }
 }
